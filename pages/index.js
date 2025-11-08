672f47e73bae28a412faafb95ac783708bc69ad3
Alyson <elevage-lve@orange.fr>
2025-11-08 22:29:43 +0100

    Test : mise à jour du texte d'accueil

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main style={{ textAlign: 'center', padding: '50px' }}>
       <h1>Bienvenue à l’élevage De la Vallée d'Eska 🐾</h1>
-      <p>Site en cours de mise en ligne — version test.</p>
+      <p>Nouvelle version test déployée !.</p>
     </main>
   );
 }
