3dc63e4df9bd41125cb98b69c03a80bf4a593ca9
Alyson <elevage-lve@orange.fr>
2025-11-10 22:46:23 +0100

    Nettoyage du next.config.js (suppression de experimental.appDir)

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: { domains: ['images.unsplash.com', 'picsum.photos', 'res.cloudinary.com'] }
+  images: {
+    domains: ['images.unsplash.com', 'picsum.photos', 'res.cloudinary.com'],
+  },
+  // 🚀 Indique que ton code est dans /src
+  distDir: '.next',
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
 }
+
 module.exports = nextConfig
