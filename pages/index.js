15097fc1b3dabb982e99e8640e6d8b7775052acd
Alyson <elevage-lve@orange.fr>
2025-11-11 19:13:02 +0100

    Suppression du doublon index.js et préparation déploiement

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ textAlign: 'center', padding: '50px' }}>
-      <h1>Bienvenue à l’élevage De la Vallée d'Eska 🐾</h1>
-      <p>Nouvelle version test déployée !.</p>
-    </main>
-  );
-}
